2b7f2d68fec2b020c2adcde2655e60e8e710ed24
HiteshKochar22104074 <hiteshkochar.bt22ele@pec.edu.in>
2024-07-04 01:45:15 +0530

    E-Commerce

---
+++ frontend/src/redux/api/usersApiSlice.js
@@ -0,0 +1,27 @@
+import { apiSlice } from "./apiSlice";
+import { USERS_URL } from "../constants";
+import { logout } from "../features/auth/authSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/auth`,
+                method: "POST",
+                body: data,
+            }),
+        }),
+
+        logout: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/logout`,
+                method: "POST",
+            }),
+        }),
+    }),
+});
+
+export const { 
+    useLoginMutation,
+    useLogoutMutation
+} = usersApiSlice
